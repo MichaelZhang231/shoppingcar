10d38c47df61975d43c78753b66db661c8b8d572
unknown <1394544459@qq.com>
2019-12-28 19:26:24 +0800

    init

---
+++ static/js/0.fa5b0a959f04ddcba6a8.js
@@ -0,0 +1,2 @@
+webpackJsonp([0],{UJfa:function(e,n){},q784:function(e,n,t){"use strict";var r={render:function(){var e=this.$createElement;return(this._self._c||e)("h1",[this._v("This is about Page")])},staticRenderFns:[]};n.a=r},zRCK:function(e,n,t){"use strict";Object.defineProperty(n,"__esModule",{value:!0});var r=t("UJfa"),a=t.n(r);for(var u in r)"default"!==u&&function(e){t.d(n,e,function(){return r[e]})}(u);var i=t("q784"),s=t("VU/8")(a.a,i.a,!1,null,null,null);n.default=s.exports}});
+//# sourceMappingURL=0.fa5b0a959f04ddcba6a8.js.map
